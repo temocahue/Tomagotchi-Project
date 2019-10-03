c6536af2fe1250534eceb20ea5a8611126886a44
Huactemo Cahue <temocahue@yahoo.com>
2019-10-03 14:33:19 -0500

    feedtamagotchiworks

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ class Tamagotchi {
 		this.name = name
 
 	}
+
 }
 
 const game = {
@@ -14,6 +15,8 @@ const game = {
 	pet: null,
 	timePassed: 0,
 	isAlive: true,
+
+	// keep track of lights off --- use boolean
 
 	start() {
 
@@ -44,8 +47,9 @@ const game = {
 			if(this.timePassed % 1.5 === 0){
 				this.pet.boredom += 1
 			}
-			if(this.timePassed) % 3 === 0{
-				this.pet.sleepiness 
+			if(this.timePassed % 3 === 0){
+				// add logic here -- sleepiness should go down when lights are off
+				this.pet.sleepiness += 1
 			}
 			if(this.pet.hunger > 10 || this.pet.boredom > 10 || this.pet.sleepiness > 10) {
 				clearInterval(timer)
@@ -53,50 +57,62 @@ const game = {
 			} 
 			
 
-			// 1. hungrier
-
-			// 2. bored
-
-
-			/// 4. sleepier -- lightsOff()
-
-
-
 			this.printData()
 		}, 1000)
 		
 	},
 
+	// call this anytime the values change
 	printData(){
-			
-			const $timer = $('#timer');
-			const $age = $('#age');	
-			const $hunger = $('#hunger');
-			const $boredom = $('#boredom')
-			const 
-
-			$timer.text(`timer: ${this.timePassed}s`)
-			$age.text(`age: ${this.pet.age}`)
-			$hunger.text(`hunger: ${this.pet.hunger}`)
-			$boredom.text(`boredom: ${this.pet.boredom}`)
 		
+		const $timer = $('#timer');
+		const $age = $('#age');	
+		const $hunger = $('#hunger');
+		const $boredom = $('#boredom')
+		const $sleepiness = $('#sleep')
+
+		$timer.text(`timer: ${this.timePassed}s`)
+		$age.text(`age: ${this.pet.age}`)
+		$hunger.text(`hunger: ${this.pet.hunger}`)
+		$boredom.text(`boredom: ${this.pet.boredom}`)
+		$sleepiness.text(`sleepiness: ${this.pet.sleepiness}`)
+	
 	},
 
+	turnOffLights() {
+		// css of body -- 
+		// change boolean
+	},
+	
+	feedTamagotchi() {
+		this.pet.hunger -= 1
+		this.printData()
+
+	},
+	playWithTamagotchi(){
+		game.pet.boredom -= 1
+		this.printData()
 
 
+	}
 
 	endGame() {
 		// switch this.isAlive to false
 		// tell user game is over
 		// reset values
 		// be sassy
-	}
+	
+
+
+}
+
+	
+
 
 }
 
 
 
-// $('#feed') -- on click, call method in game that reduces hunger
 
 $('#start').on('click', () => {
 	console.log('button works!!!!!!!!!!!');
@@ -107,22 +123,17 @@ $('#start').on('click', () => {
 })
 
 $('#feed').on('click', () => {
-	game.pet.hunger -= 1
-	console.log('feed it works');
-
-
+	game.feedTamagotchi()
 })
 
 $('#light').on('click', () => {
-
+	game.pet.sleepiness -= 2
 	console.log('light it works');
-
-
 })
 
 $('#play').on('click', () => {
+	
 	console.log('play it works');
-
 
 })
 
